bdf4346c7993ef005a6e76d8733c2ba6766edb26
pkd2210 <45825608+pkd2210@users.noreply.github.com>
2026-02-25 20:32:44 +0200

    Add category

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
     import config from '$lib/stores/config.json';
     import { onMount } from 'svelte';
+    import { page } from '$app/stores';
     import Card from '$lib/components/card.svelte';
 
     export let data;
     let items: any[] = [];
+    let allItems: any[] = [];
+    $: category = $page.url.searchParams.get('category') || 'all';
+    $: items = category === 'all' ? allItems : allItems.filter(item => {
+        return item.Category && Array.isArray(item.Category) && item.Category.includes(category);
+    });
+
 
     onMount(async () => {
-        const response = await fetch('/shop/items');
+        const response = await fetch(`/shop/items`);
         if (response.ok) {
             const data = await response.json();
-            items = Array.isArray(data) ? data : [data];
+            allItems = Array.isArray(data) ? data : [data];
         } else {
             console.error('Failed to fetch items:', response.statusText);
         }
@@ -27,7 +34,19 @@
         {/if}
         <div class="subtitle" style="opacity: 0.7;">* Fullfillment will be based on local prices & availability.</div>
     </section>
-    
+    <section class="flex flex-col items-center justify-center gap-4">
+        <div class="flex flex-wrap items-center justify-center gap-4">
+            <a href="?category=all" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">All</a>
+            <a href="?category=software" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Software</a>
+            <a href="?category=audio" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Audio Recording</a>
+            <a href="?category=video-editing" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Video Editing</a>
+            <a href="?category=audio-editing" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Audio Editing</a>
+            <a href="?category=photography" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Photography</a>
+            <a href="?category=storage" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Storage</a>
+            <a href="?category=accessories" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Accessories</a>
+            <a href="?category=streaming" class="px-4 py-2 bg-transparent text-[var(--theme-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow border stroke-[var(--theme-color)]">Streaming</a>
+        </div>
+    </section>
     <section class="cards-container">
         {#each items as item}
             <Card {item} {data} />
