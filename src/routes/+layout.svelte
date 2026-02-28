4bf5c8b77213e3f00fc1fef6aa67ea8034d2d28c
pkd2210 <45825608+pkd2210@users.noreply.github.com>
2026-02-28 14:30:03 +0200

    Add Favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
     import config from '$lib/stores/config.json';
 	
 	let { children, data } = $props();
@@ -28,7 +27,12 @@
 </div>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="TrailIt YSWS" />
+	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 
 <div
