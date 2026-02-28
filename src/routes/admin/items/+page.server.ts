1426704aa6e73e0ed73c06ec1b5d07b4dd528b99
pkd2210 <45825608+pkd2210@users.noreply.github.com>
2026-02-28 19:03:18 +0200

    Update passkey to work

---
+++ src/routes/admin/items/+page.server.ts
@@ -0,0 +1,5 @@
+export const load = async () => {
+    return {
+        filloutPasskey: process.env.FILLOUT_PASSKEY
+    };
+}
--- src/routes/admin/items/+page.svelte
+++ src/routes/admin/items/+page.svelte
@@ -1,15 +1,13 @@
 <script>
     import config from '$lib/stores/config.json';
     let { data } = $props();
-    import dotenv from 'dotenv';
-    dotenv.config();
 </script>
 
 <div class="flex flex-col gap-4">
     <section class="flex flex-col items-center justify-center gap-4">
         <div class="title">{config["ysws-name"]}'s Item Management</div>
         <div class="flex flex-wrap items-center justify-center gap-4">
-            <a target="_blank" href="https://dog-match.fillout.com/trailit-itemadd?passkey=${process.env.FILLOUT_PASSKEY}" class="px-4 py-2 bg-[var(--theme-color)] text-[var(--background-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow">Create a new item</a>
+            <a target="_blank" href="https://dog-match.fillout.com/trailit-itemadd?passkey={data.filloutPasskey}" class="px-4 py-2 bg-[var(--theme-color)] text-[var(--background-color)] rounded-lg shadow-md hover:shadow-lg transition-shadow">Create a new item</a>
         </div>
     </section>
 </div>
